5e651be3e8b96efaa78ccd40ddb65923b9d29a36
397349994 <397349994@qq.com>
2021-07-29 16:40:47 +0800

    header document

---
+++ hotel.h
@@ -0,0 +1,17 @@
+#ifndef HOTEL_H_INCLUDED
+#define HOTEL_H_INCLUDED
+
+#define HOTEL1 872.0
+#define HOTEL2 872.0
+#define HOTEL3 872.0
+#define HOTEL4 872.0
+#define DISCOUNT 0.95
+
+int menu(void);
+int getNights(void);
+int showPrice(int nights);
+
+
+
+
+#endif // HOTEL_H_INCLUDED
